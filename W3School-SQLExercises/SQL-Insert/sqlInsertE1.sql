39048fbc68ebd876fcb91c341fb1bb82e50358d7
Alexoah YT <alexandra.soah@gmail.com>
2020-04-02 15:46:59 +0700

    added answer files for SQL Insert exercises

---
+++ W3School-SQLExercises/SQL-Insert/sqlInsertE1.sql
@@ -0,0 +1,32 @@
+/**
+ *	from SQL Insert: Exercise 1 ( https://www.w3schools.com/sql/exercise.asp?filename=exercise_insert1 )
+ *
+ *	question:
+ *	Insert a new record in the Customers table.
+ *
+ *	___________ Customers _
+ *	CustomerName, 
+ *	Address, 
+ *	City, 
+ *	PostalCode,
+ *	Country_
+ *	______ _
+ *	'Hekkan Burger',
+ *	'Gateveien 15',
+ *	'Sandnes',
+ *	'4306',
+ *	'Norway'_;
+ *
+ */
+INSERT INTO Customers (
+CustomerName, 
+Address, 
+City, 
+PostalCode,
+Country)
+VALUES (
+'Hekkan Burger',
+'Gateveien 15',
+'Sandnes',
+'4306',
+'Norway');
