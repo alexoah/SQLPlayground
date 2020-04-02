c95cd63c0d7a41c890fd1b67b826f39ebdea90c6
Alexoah YT <alexandra.soah@gmail.com>
2020-04-02 15:53:35 +0700

    added answer files for SQL Delete exercises

---
+++ W3School-SQLExercises/SQL-Delete/sqlDeleteE1.sql
@@ -0,0 +1,12 @@
+/**
+ *	from SQL Delete: Exercise 1 ( https://www.w3schools.com/sql/exercise.asp?filename=exercise_delete1 )
+ *
+ *	question:
+ *	Delete all the records from the Customers table where the Country value is 'Norway'.
+ *
+ *	___________ Customers
+ *	_____ Country = 'Norway';
+ *
+ */
+DELETE FROM Customers
+WHERE Country = 'Norway';
---
+++ W3School-SQLExercises/SQL-Delete/sqlDeleteE2.sql
@@ -0,0 +1,10 @@
+/**
+ *	from SQL Delete: Exercise 2 ( https://www.w3schools.com/sql/exercise.asp?filename=exercise_delete2 )
+ *
+ *	question:
+ *	Delete all the records from the Customers table.
+ *
+ *	___________ Customers;
+ *
+ */
+DELETE FROM Customers;
